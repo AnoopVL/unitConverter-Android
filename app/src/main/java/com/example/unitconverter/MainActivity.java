076c4ef0218c2c5c05cb6b8a15a312b1486377a4
AnoopVL <avlanjekar4@gmail.com>
2024-01-11 19:53:51 +0530

    Toast Added

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "On click listener worked !!", Toast.LENGTH_SHORT).show();
             }
         });
 
