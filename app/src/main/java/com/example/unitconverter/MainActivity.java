7312b31f7f4d18d61fdf74c484b2558a0da45b2f
AnoopVL <avlanjekar4@gmail.com>
2024-01-13 22:35:10 +0530

    hours Converter done

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,11 +23,24 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView2);
         editText = findViewById(R.id.editTextText);
         button.setOnClickListener(new View.OnClickListener() {
+           @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "On click listener worked !!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Conversion Completed", Toast.LENGTH_SHORT).show();
+                String dayIP = editText.getText().toString();
+                int days = Integer.parseInt(dayIP);
+                int hours = days*24;
+                String hoursOP = String.valueOf(hours);
+                textView.setText("Total hours in "+days+" days is : "+hoursOP);
             }
         });
-
     }
+//    private void hideKeyboard() {
+//        View view = this.getCurrentFocus();
+//        if (view != null) {
+//            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+//        }
+//    }
+
 }
